3360db53b62362318299041e53f782a134bd7ab3
Buruvuru <carrotgee@gmail.com>
2023-02-05 00:40:31 +0200

    Few functions 01

---
+++ quickerTool.py
@@ -0,0 +1,24 @@
+#function to set the Doctype
+def doctypeTag():
+    tag="<!DOCTYPE html>"
+    return tag
+
+#opening html tag
+def openingHtmlTag():
+    tag="<html>"
+    return tag
+
+#closing html tag
+def closingHtmlTag():
+    tag="</html>"
+    return tag
+
+#html opening head tag
+def openingHeadTag():
+    tag="<head>"
+    return tag
+
+#html closing head tag
+def closingHeadTag():
+    tag="</head>"
+    return tag
