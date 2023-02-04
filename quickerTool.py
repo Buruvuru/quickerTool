b234e57978f865220ee1311db2b21042bcf3f2e2
Buruvuru <carrotgee@gmail.com>
2023-02-05 01:29:22 +0200

    Few functions 02

--- quickerTool.py
+++ quickerTool.py
@@ -22,3 +22,30 @@ def openingHeadTag():
 def closingHeadTag():
     tag="</head>"
     return tag
+
+#html opening tag
+def openingBodyTag():
+    tag="<body>"
+    return tag
+
+#html closing body tag
+def closingBodyTag():
+    tag="</body>"
+    return tag
+
+#html attributes ##########################################################3
+
+#a tag with link attribute
+def ayTag(link,linkLabel):
+    tag="<a href="+link+">"+linkLabel+"</a>"
+    return tag
+
+# img tag with src attribute
+def imgTag(source):
+    tag="<img src="+source+">"
+    return tag
+
+
+
+newtag=imgTag("img_girl.jpg")
+print(newtag)
