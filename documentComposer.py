07967b8c3da69b726e22b1218a705ba6d819ac58
Buruvuru <carrotgee@gmail.com>
2023-02-10 22:23:07 -0800

    interesting stuff going on here

--- documentComposer.py
+++ documentComposer.py
@@ -1,18 +1,38 @@
 from quickerTool import *
 import os
 #function to set name of the path
-def makeFolder():
-    path=input("To begin, please enter the path to your project folder")
+
+#default projects folder
+def defaultPath():
+    defpath="C:/qtoprojects"
+    return defpath
+
+#function to make default path
+def makeDefPath(defpath):
+    path = defpath
     os.mkdir(path)
 
-#function to verify if path exists
+#function to create a folder(excluding default path)
+def makeFolder(folderName):
+    path=folderName
+    os.mkdir(path)
+    return path
+
+#function to verify if path exists(can also be used to verify defpath)
 def verifyPath(path):
     isExist=os.path.exists(path)
     return isExist
 
-def makePageSource(name):
-    file=open(name+".txt")
+#function to make a file
+def makePageSource(name,defpath,path):
+    filename=os.path.join(defpath,path,name)
+    f=open(filename,"w")
 
 
+#PROGRAM ENTRY POINT
+projectsFolder=defaultPath()
+doesItExist=verifyPath(projectsFolder)
+if doesItExist is True:
 
 
+newPage=makePageSource("indeca")
