84e79bec003c75cb2bf9af836ba0f462b7adf701
Buruvuru <carrotgee@gmail.com>
2023-02-16 13:47:11 -0800

    Turning point. No more text files to be used. The program now uses a shell to communicate with the developer

--- compiler.py
+++ compiler.py
@@ -1,8 +1,31 @@
-from constants import sendTagStream
-#while True:
-    #with open("welcome.html",'r+') as f:
-        #contents = f.readlines()
-        #print(contents)
+from quickerTool import *
+while True:
+    text=input('quickerTool Command > :')
+    print(text)
 
-recievedStream=sendTagStream()
-str.join(recievedStream)
+    #Container for generated html
+    htdoc=[]
+    #Set Language constants
+
+    #compare language constants with entered text.
+    if text=="doctype":
+        doctype = doctypeTag()
+        htdoc.append(doctype)
+    elif text=="title":
+        title = titleTag(input("Title of your page"))
+        htdoc.append(title)
+    elif text=="htmlopen":
+        htmlopen=openingHeadTag()
+        htdoc.append(htmlopen)
+    elif text=="htmlclose":
+        htmlclose=closingHtmlTag()
+        htdoc.append(htmlclose)
+
+    elif text=="para":
+        para=paraTag(input("Type Paragraph "))
+        htdoc.append(para)
+
+print(htdoc)
+
+
+
